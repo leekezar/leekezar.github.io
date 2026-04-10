0ac87b62878ff4f53fbb3d214e117784b4cace2e
Lee Kezar <lkezar@usc.edu>
2026-04-10 10:11:47 -0400

    Add pack-based STEM annotation dashboard

---
+++ asl-dashboard/stem-annotation/assets/assignments.js
@@ -0,0 +1,20 @@
+window.STEM_ANNOTATION_ASSIGNMENTS = {
+  "pins": {},
+  "construct_options": [
+    "lexical_sign",
+    "fingerspelling",
+    "loansign",
+    "name_sign",
+    "number_sign",
+    "compound",
+    "gesture",
+    "body",
+    "body_part",
+    "descriptive",
+    "instrument",
+    "locative",
+    "plural",
+    "semantic",
+    "neologism"
+  ]
+};
